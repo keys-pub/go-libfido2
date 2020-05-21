c6d405a7624a5c97090bc30d396a9566c9100b25
Gabriel Handford <gabrielh@gmail.com>
2020-05-21 12:08:29 -0700

    darwin: Use installed dependencies

--- fido2.go
+++ fido2.go
@@ -1,8 +1,8 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit ${SRCDIR}/darwin/lib/libfido2.a ${SRCDIR}/darwin/lib/libssl.a ${SRCDIR}/darwin/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
-#cgo darwin CFLAGS: -I${SRCDIR}/darwin/include
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl/lib/libssl.a /usr/local/opt/openssl/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
+#cgo darwin CFLAGS: -I/usr/local/include -I/usr/local/opt/openssl/include
 #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu -lfido2
 #cgo linux CFLAGS: -I/usr/include/fido
 #cgo windows LDFLAGS: -L${SRCDIR}/windows/lib -lfido2
