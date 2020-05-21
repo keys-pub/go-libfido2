192e4cceec124f597dab782a0a9d6a9b71b0b970
Gabriel Handford <gabrielh@gmail.com>
2020-05-21 12:30:46 -0700

    Fix

--- fido2.go
+++ fido2.go
@@ -1,8 +1,8 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a ${SRCDIR}/darwin/lib/libcbor.a /usr/local/opt/openssl/lib/libcrypto.a
-#cgo darwin CFLAGS: -I/usr/local/include -I/usr/local/opt/openssl/include
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/Cellar/libfido2/1.4.0_2/lib/libfido2.a /usr/local/Cellar/openssl@1.1/1.1.1g/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
+#cgo darwin CFLAGS: -I/usr/local/Cellar/libfido2/1.4.0_2/include -I/usr/local/Cellar/openssl@1.1/1.1.1g/include
 #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu -lfido2
 #cgo linux CFLAGS: -I/usr/include/fido
 #cgo windows LDFLAGS: -L${SRCDIR}/windows/lib -lfido2
