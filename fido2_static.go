b0cf1acf0422891a01b80f6c71b66f5a7ae297b9
Gabriel Handford <gabrielh@gmail.com>
2020-09-14 12:58:24 -0700

    Update fido2_static.go

--- fido2_static.go
+++ fido2_static.go
@@ -1,7 +1,7 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/Cellar/libfido2/1.5.1/lib/libfido2.a /usr/local/Cellar/openssl@1.1/1.1.1g/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/Cellar/libfido2/1.5.0/lib/libfido2.a /usr/local/Cellar/openssl@1.1/1.1.1g/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
 #cgo darwin CFLAGS: -I/usr/local/Cellar/libfido2/1.5.0/include -I/usr/local/Cellar/openssl@1.1/1.1.1g/include
 */
 import "C"
