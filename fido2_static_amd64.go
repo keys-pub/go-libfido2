d050f4def3cc4b45de48c07bedc24c4accf78b69
Tyler Davis <tylerd@canva.com>
2022-03-04 09:21:06 +1100

    Working build on darwin/arm64

---
+++ fido2_static_amd64.go
@@ -0,0 +1,7 @@
+package libfido2
+
+/*
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/amd64/lib/libcbor.a
+#cgo darwin CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@1.1/include
+*/
+import "C"
