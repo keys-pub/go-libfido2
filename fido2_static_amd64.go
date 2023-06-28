281619cff413091db4064147b8b38a57744fc2dd
Tyler Davis <tylerd@canva.com>
2023-06-28 12:55:03 +1000

    link against openssl3 on darwin

--- fido2_static_amd64.go
+++ fido2_static_amd64.go
@@ -1,7 +1,7 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/amd64/lib/libcbor.a
-#cgo darwin CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@1.1/include
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@3/lib/libcrypto.a ${SRCDIR}/darwin/amd64/lib/libcbor.a
+#cgo darwin CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@3/include
 */
 import "C"
--- fido2_static_arm64.go
+++ fido2_static_arm64.go
@@ -1,7 +1,7 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /opt/homebrew/opt/libfido2/lib/libfido2.a /opt/homebrew/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/arm64/lib/libcbor.a
-#cgo darwin CFLAGS: -I/opt/homebrew/opt/libfido2/include -I/opt/homebrew/opt/openssl@1.1/include
+#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /opt/homebrew/opt/libfido2/lib/libfido2.a /opt/homebrew/opt/openssl@3/lib/libcrypto.a ${SRCDIR}/darwin/arm64/lib/libcbor.a
+#cgo darwin CFLAGS: -I/opt/homebrew/opt/libfido2/include -I/opt/homebrew/opt/openssl@3/include
 */
 import "C"
